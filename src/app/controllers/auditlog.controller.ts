5c317c15030d8ee89aeef9190ffac0f1cf0de79d
John Nabil <johnnabil85@gmail.com>
2025-11-13 15:27:08 +0200

    adding audit logs api

--- src/app/controllers/auditlog.controller.ts
+++ src/app/controllers/auditlog.controller.ts
@@ -9,6 +9,43 @@ export class AuditlogController {
     }
 
     async list(Request: FastifyRequest, Response: FastifyReply) {
-        return await this.auditLogRepo.list();
+        const {page = 1, limit = 10, user_id, transaction_id} = Request.query as {
+            page?: number;
+            limit?: number;
+            user_id?: number;
+            transaction_id?: string;
+        };
+        let totalPages = 0;
+        try {
+            const offset = (page - 1) * limit;
+
+            const {rows: logs, count: total} = await this.auditLogRepo.list({
+                user_id: user_id,
+                transaction_id: transaction_id,
+            }, offset, limit);
+
+            totalPages = Math.ceil(total / limit);
+
+            return Response.send({
+                data: logs,
+                meta: {
+                    total,
+                    page,
+                    limit,
+                    totalPages,
+                },
+            });
+        } catch (error: any) {
+            console.log(error);
+            return Response.send({
+                data: [],
+                meta: {
+                    total: 0,
+                    page,
+                    limit,
+                    totalPages,
+                },
+            });
+        }
     };
 }
